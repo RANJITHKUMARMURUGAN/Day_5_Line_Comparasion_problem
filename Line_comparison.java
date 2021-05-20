3567a0b0526acadb25b97e5589be6f0a75fec702
RANJITHKUMAR <ranjithm1193@gmail.com>
2021-05-20 15:55:18 +0530

    [Ranjith] modified Line_comparison.java

--- Line_comparison.java
+++ Line_comparison.java
@@ -14,5 +14,17 @@ public class Line_comparison {
 		y2=scan.nextDouble();
 		length_of_line_1=Math.sqrt((Math.pow((x2-x1),2)) + (Math.pow((y2-y1),2)) );
 		System.out.println("Length of the Line one : "+" "+length_of_line_1);
+
+		System.out.println("Enter the value of x1 = ");
+		x1=scan.nextDouble();
+		System.out.println("Enter the value of x2 = ");
+		x2=scan.nextDouble();
+		
+		System.out.println("Enter the value of y1 = ");
+		y1=scan.nextDouble();
+		System.out.println("Enter the value of y2 = ");
+		y2=scan.nextDouble();
+		length_of_line_2=Math.sqrt((Math.pow((x2-x1),2)) + (Math.pow((y2-y1),2)) );
+		System.out.println("Length of the Line one : "+" "+length_of_line_2);
 	}
 }
